6486e697e79ac1f67f10d166760a169ee79edace
RivoAnnafiHakim <149139735+RivoAnnafiHakim@users.noreply.github.com>
2023-10-27 21:59:15 +0700

    Update orderpizza.py
    menambahkan codingan ukuran pizza

--- orderpizza.py
+++ orderpizza.py
@@ -27,8 +27,23 @@ print("- CheesyBites")
 print("- Crowncrust")
 print("- StuffedCrust")
 
-crust = int(input("Masukkan pilihan crust pizza Anda: "))
+crust = str(input("Masukkan pilihan crust pizza Anda: "))
 
 if crust in ['Pan Pizza', 'CheesyBites', 'Crowncrust', 'StuffedCrust']:
     harga_crust = 12000
 else: print("Pilihan Anda tidak valid. Silahkan ulangi program.")
+
+print("Silahkan pilih ukuran pizza Anda: ")
+print("- Personal")
+print("- Medium")
+print("- Large") 
+
+Ukuran = str(input("Masukkan pilihan Ukuran pizza Anda: "))
+
+if Ukuran == "Personal":
+ harga_ukuran = 10000
+elif Ukuran == "Medium":
+  harga_ukuran = 15000
+elif Ukuran == "Large":
+  harga_ukuran = 20000
+else: print("Ukuran yang anda masukkan tidak valid.")
