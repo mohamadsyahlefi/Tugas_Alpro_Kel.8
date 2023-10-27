2c60d23405c798ad62109bbcad65b471af97d9ac
Mohamad Syahlefi G. A. M <140249381+mohamadsyahlefi@users.noreply.github.com>
2023-10-27 20:06:41 +0700

    Update orderpizza.py

--- orderpizza.py
+++ orderpizza.py
@@ -4,3 +4,31 @@ print('- Frankfurter')
 print('- Meat Monsta')
 print('- Super Supreme')
 print('- Super Supreme Chicken')
+
+topping = str(input('Masukkan menu pilihan anda: '))
+
+if topping == 'Frankfurter':
+    nama_topping = 'Frankfurter'
+    
+elif topping == 'Meat Monsta':
+    nama_topping = 'Meat Monsta'
+    
+elif topping == 'Super Supreme':
+    nama_topping = 'Super Supreme'
+    
+elif topping == 'Super Supreme Chicken':
+    nama_topping = 'Super Supreme Chicken'
+else :
+    print("Pilihan Anda tidak valid. Silahkan ulangi program.")
+
+print("Silahkan pilih crust pizza Anda: ")
+print("- Pan Pizza")
+print("- CheesyBites")
+print("- Crowncrust")
+print("- StuffedCrust")
+
+crust = int(input("Masukkan pilihan crust pizza Anda: "))
+
+if crust in ['Pan Pizza', 'CheesyBites', 'Crowncrust', 'StuffedCrust']:
+    harga_crust = 12000
+else: print("Pilihan Anda tidak valid. Silahkan ulangi program.")
